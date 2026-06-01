1d404e072ff11b0a14f233472511e43af8a2b515
Keisuke KUDO <151166401+Apo-Matchbox@users.noreply.github.com>
2026-06-01 12:20:49 -0400

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.java
@@ -0,0 +1,22 @@
+// HashMap (One pass)
+// revised the variable names
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {        
+        Map<Integer, Integer> seenNumToIndex = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int complement = target - num;
+
+            if (seenNumToIndex.containsKey(complement)) {
+                return new int[] {seenNumToIndex.get(complement), i};
+            }
+            seenNumToIndex.put(num, i);
+        }
+        return new int[0];
+    }
+}
+
+//T  O(n)
+//M  O(n)
