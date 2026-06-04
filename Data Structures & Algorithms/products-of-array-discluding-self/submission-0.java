d2a896b5edd53f68fa439332a00532fc4ef7834d
Keisuke KUDO <151166401+Apo-Matchbox@users.noreply.github.com>
2026-06-04 12:59:31 -0400

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.java
@@ -0,0 +1,22 @@
+//Brute Force
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+       int n = nums.length;
+       int[] result = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int product = 1;
+            for (int j = 0; j < n; j++) {
+                if (i != j) {
+                    product *= nums[j];
+                }
+            }
+            result[i] = product;
+        }
+        return result;
+    }
+}  
+
+// T: O(n^2)
+// T: O(1) extra space and O(n) for output array
