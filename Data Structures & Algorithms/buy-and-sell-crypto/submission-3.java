c9ec01db3c38a51cf53cae5930378e15e39dfb8d
Keisuke KUDO <151166401+Apo-Matchbox@users.noreply.github.com>
2026-06-12 14:21:44 -0400

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.java
@@ -0,0 +1,17 @@
+// DP
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxP = 0;
+        int minBuy = prices[0];
+
+        for (int sell : prices) {
+            maxP = Math.max(maxP, sell - minBuy);
+            minBuy = Math.min(minBuy, sell);
+        }
+        return maxP;
+    }
+}
+
+// O(n)
+// O(1)
